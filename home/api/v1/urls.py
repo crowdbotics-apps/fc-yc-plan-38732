65d54ea19dd1292be36c279f63487a1a62d0aea1
crowdbotics-apps <team@crowdbotics.com>
2023-02-06 07:23:58 +0000

    Update models

--- home/api/v1/urls.py
+++ home/api/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .viewsets import CourseViewSet
 
 from home.api.v1.viewsets import (
     SignupViewSet,
@@ -9,6 +10,7 @@ from home.api.v1.viewsets import (
 router = DefaultRouter()
 router.register("signup", SignupViewSet, basename="signup")
 router.register("login", LoginViewSet, basename="login")
+router.register('course', CourseViewSet )
 
 urlpatterns = [
     path("", include(router.urls)),
--- home/models.py
+++ home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Course(models.Model):
+    'Generated Model'
+    courseName = models.TextField()
+    courseId = models.UUIDField(null=True,blank=True,)
